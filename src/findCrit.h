78f4b488db33b912e7b2dc329564fed809c167af
RayHE <rayhe88@gmail.com>
2019-11-06 03:14:48 -0500

    Uncommit bondpath and change the tolerance in gradient 1.E-7

--- src/findCrit.h
+++ src/findCrit.h
@@ -16,8 +16,8 @@
 
  #define TOLFUN0  1.E-6 //original
  #define TOLFUN  1.E-5
- #define TOLGRD  1.E-14
- //#define TOLGRD  1.E-7 // original
+ // #define TOLGRD  1.E-14
+ #define TOLGRD  1.E-7 // original
  #define TOLNRM  100.
  #define MAXITER1 30
  #define MAXITER2 300
