94b9f47762f54925c12203e495da00dd913d2ce9
RayHE <rayhe88@gmail.com>
2020-04-20 20:55:58 -0400

    Add test for interpolatores

---
+++ src/numGradLines.h
@@ -0,0 +1,32 @@
+/** 
+ * @file   numGradLines.h
+ * @brief
+ * @author Raymundo Hernandez-Esparza.
+ * @date   April 2020.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "findCrit.h"
+
+#include "struct.h"
+
+#ifndef _NUMGRADLINES_H_
+ #define _NUMGRADLINES_H_
+
+ #define STEP_EPS 0.005
+
+  int gradientLines(int, dataCritP *, dataCube, dataRun, double, const double *,
+  char *);
+  int gradientLines2(int, dataCritP *, dataCube, dataRun, double, const double *,
+  char *);
+  int gradientLines3(int, int, int, dataCritP *, dataCritP *, dataCritP *, dataCube, dataRun, double, const double *,
+  char *);
+
+  int gradientLines4(int, int, int, dataCritP *, dataCritP *, dataCritP *, dataCube, dataRun, double, const double *,
+  char *);
+  int sortBondCritP(int, double[3], dataCritP *);
+  void cpyCritP(int, dataCritP*, dataCritP*);
+#endif
