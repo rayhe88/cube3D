60380292a222f21ad17ba2ea33c12f76d7480610
RayHE <rayhe88@gmail.com>
2021-08-25 18:14:13 -0500

    Rename loadRunCommands to runCommands

---
+++ src/runCommands.h
@@ -0,0 +1,16 @@
+/**
+ *  @file   loadRunCommands.h
+ *  @brief  header file for loading run commands.
+ *
+ *  @author Raymundo Hernández-Esparza.
+ *  @date   August 2021.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef _LOAD_RUN_COMMANDS_H_
+#define _LOAD_RUN_COMMANDS_H_
+
+
+
+#endif
